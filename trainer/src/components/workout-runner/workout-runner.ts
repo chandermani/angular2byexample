162e7f611bd457002918a699310b64d9b3fc2fdf
chandermani <chandermani@outlook.com>
2015-11-11 10:14:30 +0530

    Implemented audio support for workout

--- trainer/src/components/workout-runner/workout-runner.ts
+++ trainer/src/components/workout-runner/workout-runner.ts
@@ -1,13 +1,14 @@
-import {Component, CORE_DIRECTIVES} from 'angular2/angular2';
+import {Component, CORE_DIRECTIVES, ViewChild, EventEmitter, Output} from 'angular2/angular2';
 import {WorkoutPlan, ExercisePlan, Exercise, ExerciseRelated} from './model';
 import {ExerciseDescription} from './exercise-description';
 import {VideoPlayer} from './video-player';
+import {WorkoutAudio} from './workout-audio';
 import {SecondsToTime} from './pipes';
 
 @Component({
   selector: 'workout-runner',
   templateUrl: '/src/components/workout-runner/workout-runner.tpl.html',
-  directives: [ExerciseDescription, VideoPlayer, CORE_DIRECTIVES],
+  directives: [ExerciseDescription, VideoPlayer, WorkoutAudio, CORE_DIRECTIVES],
   pipes: [SecondsToTime]
 })
 export class WorkoutRunner {
@@ -19,6 +20,14 @@ export class WorkoutRunner {
   exerciseRunningDuration: number;
   exerciseTrackingInterval: number;
   workoutPaused: boolean;
+  @ViewChild(WorkoutAudio) workoutAudioPlayer: WorkoutAudio;
+  @Output() exercisePaused: EventEmitter = new EventEmitter();
+  @Output() exerciseResumed: EventEmitter = new EventEmitter();
+  @Output() exerciseProgress: EventEmitter = new EventEmitter();
+  @Output() exerciseChanged: EventEmitter = new EventEmitter();
+  @Output() workoutStarted: EventEmitter = new EventEmitter();
+  @Output() workoutComplete: EventEmitter = new EventEmitter();
+
 
   constructor() {
     this.workoutPlan = this.buildWorkout();
@@ -32,7 +41,7 @@ export class WorkoutRunner {
     this.workoutTimeRemaining = this.workoutPlan.totalWorkoutDuration();
     this.currentExerciseIndex = 0;
     this.startExercise(this.workoutPlan.exercises[0]);
-
+    this.workoutStarted.next(this.workoutPlan);
     /*let intervalId = setInterval(() => {
       --this.workoutTimeRemaining;
       if (this.workoutTimeRemaining == 0) clearInterval(intervalId);
@@ -42,11 +51,15 @@ export class WorkoutRunner {
   pause() {
     clearInterval(this.exerciseTrackingInterval);
     this.workoutPaused = true;
+    this.workoutAudioPlayer.stop();
+    this.exercisePaused.next(this.currentExerciseIndex);
   }
 
   resume() {
     this.startExerciseTimeTracking();
     this.workoutPaused = false;
+    this.workoutAudioPlayer.resume();
+    this.exerciseResumed.next(this.currentExerciseIndex);
   }
 
   pauseResumeToggle() {
@@ -82,13 +95,11 @@ export class WorkoutRunner {
   getNextExercise(): ExercisePlan {
     let nextExercise: ExercisePlan = null;
     if (this.currentExercise === this.restExercise) {
-      this.currentExerciseIndex++;
-      nextExercise = this.workoutPlan.exercises[this.currentExerciseIndex];
+      nextExercise = this.workoutPlan.exercises[this.currentExerciseIndex + 1];
     }
     else if (this.currentExerciseIndex < this.workoutPlan.exercises.length - 1) {
       nextExercise = this.restExercise;
     }
-
     return nextExercise;
   }
 
@@ -98,15 +109,26 @@ export class WorkoutRunner {
         clearInterval(this.exerciseTrackingInterval);
         let next: ExercisePlan = this.getNextExercise();
         if (next) {
+          if (next !== this.restExercise) {
+            this.currentExerciseIndex++;
+          }
           this.startExercise(next);
+          this.exerciseChanged.next({ current: next, next: this.getNextExercise() });
         }
         else {
+          this.workoutComplete.next(this.workoutPlan);
           console.log("Workout complete!");
         }
         return;
       }
       ++this.exerciseRunningDuration;
       --this.workoutTimeRemaining;
+      this.exerciseProgress.next({
+        exercise: this.currentExercise,
+        runningFor: this.exerciseRunningDuration,
+        timeRemaining: this.currentExercise.duration - this.exerciseRunningDuration,
+        workoutTimeRemaining: this.workoutTimeRemaining
+      });
     }, 1000);
   }
 
@@ -119,7 +141,7 @@ export class WorkoutRunner {
           "Jumping Jacks",
           "A jumping jack or star jump, also called side-straddle hop is a physical jumping exercise.",
           "JumpingJacks.png",
-          "",
+          "jumpingjacks.wav",
           `Assume an erect position, with feet together and arms at your side. <br>
           Slightly bend your knees, and propel yourself a few inches into the air. <br>
           While in air, bring your legs out to the side about shoulder width or slightly wider. <br>
@@ -135,7 +157,7 @@ export class WorkoutRunner {
           "Wall Sit",
           "A wall sit, also known as a Roman Chair, is an exercise done to strengthen the quadriceps muscles.",
           "wallsit.png",
-          "",
+          "wallsit.wav",
           `Place your back against a wall with your feet shoulder width apart and a little ways out from the wall.
           Then, keeping your back against the wall, lower your hips until your knees form right angles.`,
           new ExerciseRelated(["//www.youtube.com/embed/y-wV4Venusw", "//www.youtube.com/embed/MMV3v4ap4ro"])),
@@ -148,7 +170,7 @@ export class WorkoutRunner {
           "Push up",
           "A push-up is a common exercise performed in a prone position by raising and lowering the body using the arms",
           "pushup.png",
-          "",
+          "pushups.wav",
           `Lie prone on the ground with hands placed as wide or slightly wider than shoulder width.
           Keeping the body straight, lower body to the ground by bending arms at the elbows.
           Raise body up off the ground by extending the arms.`,
@@ -162,7 +184,7 @@ export class WorkoutRunner {
           "Abdominal Crunches",
           "The basic crunch is a abdominal exercise in a strength-training program.",
           "crunches.png",
-          "",
+          "crunches.wav",
           `Lie on your back with your knees bent and feet flat on the floor, hip-width apart.
           Place your hands behind your head so your thumbs are behind your ears.
           Hold your elbows out to the sides but rounded slightly in.
@@ -179,7 +201,7 @@ export class WorkoutRunner {
           "Step Up Onto Chair",
           "Step exercises are ideal for building muscle in your lower body.",
           "stepupontochair.png",
-          "",
+          "stepup.wav",
           `Position your chair in front of you.
           Stand with your feet about hip width apart, arms at your sides.
           Step up onto the seat with one foot, pressing down while bringing your other foot up next to it.
@@ -194,7 +216,7 @@ export class WorkoutRunner {
           "Squat",
           "The squat is a compound, full body exercise that trains primarily the muscles of the thighs, hips, buttocks and quads.",
           "squat.png",
-          "",
+          "squats.wav",
           `Stand with your head facing forward and your chest held up and out.
               Place your feet shoulder-width apart or little wider. Extend your hands straight out in front of you.
               Sit back and down like you're sitting into a chair. Keep your head facing straight as your upper body bends forward a bit. Rather than allowing your back to round, let your lower back arch slightly as you go down.
@@ -210,7 +232,7 @@ export class WorkoutRunner {
           "Tricep Dips On Chair",
           "A body weight exercise that targets triceps.",
           "tricepdips.png",
-          "",
+          "tricepdips.wav",
           `Sit up on a chair. Your legs should be slightly extended, with your feet flat on the floor.
           Place your hands edges of the chair. Your palms should be down, fingertips pointing towards the floor.
           Without moving your legs, bring your glutes forward off the chair.
@@ -225,7 +247,7 @@ export class WorkoutRunner {
           "Plank",
           "The plank (also called a front hold, hover, or abdominal bridge) is an isometric core strength exercise that involves maintaining a difficult position for extended periods of time.",
           "Plank.png",
-          "",
+          "plank.wav",
           `Get into pushup position on the floor.
           Bend your elbows 90 degrees and rest your weight on your forearms.
           Your elbows should be directly beneath your shoulders, and your body should form a straight line from head to feet.
@@ -240,7 +262,7 @@ export class WorkoutRunner {
           "High Knees",
           "A form exercise that develops strength and endurance of the hip flexors and quads and stretches the hip extensors.",
           "highknees.png",
-          "",
+          "highknees.wav",
           `Start standing with feet hip-width apart.
           Do inplace jog with your knees lifting as much as possible towards your chest.`,
           new ExerciseRelated(["//www.youtube.com/embed/OAJ_J3EZkdY", "//www.youtube.com/embed/8opcQdC-V-U"])),
@@ -253,7 +275,7 @@ export class WorkoutRunner {
           "Lunges",
           "Lunges are a good exercise for strengthening, sculpting and building several muscles/muscle groups, including the quadriceps (or thighs), the gluteus maximus (or buttocks) as well as the hamstrings.",
           "lunges.png",
-          "",
+          "lunge.wav",
           `Start standing with feet hip-width apart.
               Do inplace jog with your knees lifting as much as possible towards your chest.`,
           new ExerciseRelated(["//www.youtube.com/embed/Z2n58m2i4jg"])),
@@ -266,7 +288,7 @@ export class WorkoutRunner {
           "Pushup And Rotate",
           "A variation of pushup that requires you to rotate.",
           "pushupnrotate.png",
-          "",
+          "pushupandrotate.wav",
           `Assume the classic pushup position, but as you come up, rotate your body so your right arm lifts up and extends overhead.
           Return to the starting position, lower yourself, then push up and rotate till your left hand points toward the ceiling.`,
           new ExerciseRelated(["//www.youtube.com/embed/qHQ_E-f5278"])),
@@ -279,7 +301,7 @@ export class WorkoutRunner {
           "Side Plank",
           "A variation to Plank done using one hand only.",
           "sideplank.png",
-          "",
+          "sideplank.wav",
           `Lie on your side, in a straight line from head to feet, resting on your forearm.
           Your elbow should be directly under your shoulder.
           With your abdominals gently contracted, lift your hips off the floor, maintaining the line.
