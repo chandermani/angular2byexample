02c1d3f637a6e5502eab29d98dd29dbf4817e9b3
chandermani <chandermani@outlook.com>
2015-11-19 09:39:48 +0530

    Fixed checkpoint2.2 code.

--- trainer/src/components/workout-runner/workout-runner.ts
+++ trainer/src/components/workout-runner/workout-runner.ts
@@ -1,5 +1,5 @@
 import {Component, NgStyle} from 'angular2/angular2';
-import {WorkoutPlan, ExercisePlan, Exercise, ExerciseRelated} from './model'
+import {WorkoutPlan, ExercisePlan, Exercise} from './model'
 
 @Component({
   selector: 'workout-runner',
@@ -78,7 +78,7 @@ export class WorkoutRunner {
                             While in air, bring your legs out to the side about shoulder width or slightly wider.
                             As you are moving your legs outward, you should raise your arms up over your head; arms should be slightly bent throughout the entire in-air movement.
                             Your feet should land shoulder width or wider as your hands meet above your head with arms slightly bent`,
-          new ExerciseRelated(["//www.youtube.com/embed/dmYwZH_BNd0", "//www.youtube.com/embed/BABOdJ-2Z6o", "//www.youtube.com/embed/c4DAnQ6DtF8"])),
+          ["dmYwZH_BNd0", "BABOdJ-2Z6o", "c4DAnQ6DtF8"]),
         30));
 
     workout.exercises.push(
@@ -91,7 +91,7 @@ export class WorkoutRunner {
           "",
           `Place your back against a wall with your feet shoulder width apart and a little ways out from the wall.
           Then, keeping your back against the wall, lower your hips until your knees form right angles.`,
-          new ExerciseRelated(["//www.youtube.com/embed/y-wV4Venusw", "//www.youtube.com/embed/MMV3v4ap4ro"])),
+          ["y-wV4Venusw", "MMV3v4ap4ro"]),
         30));
 
     workout.exercises.push(
@@ -105,7 +105,7 @@ export class WorkoutRunner {
           `Lie prone on the ground with hands placed as wide or slightly wider than shoulder width.
           Keeping the body straight, lower body to the ground by bending arms at the elbows.
           Raise body up off the ground by extending the arms.`,
-          new ExerciseRelated(["//www.youtube.com/embed/Eh00_rniF8E", "//www.youtube.com/embed/ZWdBqFLNljc", "//www.youtube.com/embed/UwRLWMcOdwI", "//www.youtube.com/embed/ynPwl6qyUNM", "//www.youtube.com/embed/OicNTT2xzMI"])),
+          ["Eh00_rniF8E", "ZWdBqFLNljc", "UwRLWMcOdwI", "ynPwl6qyUNM", "OicNTT2xzMI"]),
         30));
 
     workout.exercises.push(
@@ -122,7 +122,7 @@ export class WorkoutRunner {
           Gently pull your abdominals inward.
           Curl up and forward so that your head, neck, and shoulder blades lift off the floor.
           Hold for a moment at the top of the movement and then lower slowly back down.`,
-          new ExerciseRelated(["//www.youtube.com/embed/Xyd_fa5zoEU", "//www.youtube.com/embed/MKmrqcoCZ-M"])),
+          ["Xyd_fa5zoEU", "MKmrqcoCZ-M"]),
         30));
 
     workout.exercises.push(
@@ -137,7 +137,7 @@ export class WorkoutRunner {
           Stand with your feet about hip width apart, arms at your sides.
           Step up onto the seat with one foot, pressing down while bringing your other foot up next to it.
           Step back with the leading foot and bring the trailing foot down to finish one step-up.`,
-          new ExerciseRelated(["//www.youtube.com/embed/aajhW7DD1EA"])),
+          ["aajhW7DD1EA"]),
         30));
 
     workout.exercises.push(
@@ -153,7 +153,7 @@ export class WorkoutRunner {
               Sit back and down like you're sitting into a chair. Keep your head facing straight as your upper body bends forward a bit. Rather than allowing your back to round, let your lower back arch slightly as you go down.
               Lower down so your thighs are parallel to the floor, with your knees over your ankles. Press your weight back into your heels.
               Keep your body tight, and push through your heels to bring yourself back to the starting position.`,
-          new ExerciseRelated(["//www.youtube.com/embed/QKKZ9AGYTi4", "//www.youtube.com/embed/UXJrBgI2RxA"])),
+          ["QKKZ9AGYTi4", "UXJrBgI2RxA"]),
         30));
 
     workout.exercises.push(
@@ -168,7 +168,7 @@ export class WorkoutRunner {
           Place your hands edges of the chair. Your palms should be down, fingertips pointing towards the floor.
           Without moving your legs, bring your glutes forward off the chair.
           Steadily lower yourself. When your elbows form 90 degrees angles, push yourself back up to starting position.`,
-          new ExerciseRelated(["//www.youtube.com/embed/tKjcgfu44sI", "//www.youtube.com/embed/jox1rb5krQI"])),
+          ["tKjcgfu44sI", "jox1rb5krQI"]),
         30));
 
     workout.exercises.push(
@@ -183,7 +183,7 @@ export class WorkoutRunner {
           Bend your elbows 90 degrees and rest your weight on your forearms.
           Your elbows should be directly beneath your shoulders, and your body should form a straight line from head to feet.
           Hold this position.`,
-          new ExerciseRelated(["//www.youtube.com/embed/pSHjTRCQxIw", "//www.youtube.com/embed/TvxNkmjdhMM"])),
+          ["pSHjTRCQxIw", "TvxNkmjdhMM"]),
         30));
 
     workout.exercises.push(
@@ -196,7 +196,7 @@ export class WorkoutRunner {
           "",
           `Start standing with feet hip-width apart.
           Do inplace jog with your knees lifting as much as possible towards your chest.`,
-          new ExerciseRelated(["//www.youtube.com/embed/OAJ_J3EZkdY", "//www.youtube.com/embed/8opcQdC-V-U"])),
+          ["OAJ_J3EZkdY", "8opcQdC-V-U"]),
         30));
 
     workout.exercises.push(
@@ -209,7 +209,7 @@ export class WorkoutRunner {
           "",
           `Start standing with feet hip-width apart.
               Do inplace jog with your knees lifting as much as possible towards your chest.`,
-          new ExerciseRelated(["//www.youtube.com/embed/Z2n58m2i4jg"])),
+          ["Z2n58m2i4jg"]),
         30));
 
     workout.exercises.push(
@@ -222,7 +222,7 @@ export class WorkoutRunner {
           "",
           `Assume the classic pushup position, but as you come up, rotate your body so your right arm lifts up and extends overhead.
           Return to the starting position, lower yourself, then push up and rotate till your left hand points toward the ceiling.`,
-          new ExerciseRelated(["//www.youtube.com/embed/qHQ_E-f5278"])),
+          ["qHQ_E-f5278"]),
         30));
 
     workout.exercises.push(
@@ -237,7 +237,7 @@ export class WorkoutRunner {
           Your elbow should be directly under your shoulder.
           With your abdominals gently contracted, lift your hips off the floor, maintaining the line.
           Keep your hips square and your neck in line with your spine. Hold the position.`,
-          new ExerciseRelated(["//www.youtube.com/embed/wqzrb67Dwf8", "//www.youtube.com/embed/_rdfjFSFKMY"])),
+          ["wqzrb67Dwf8", "_rdfjFSFKMY"]),
         30));
 
     return workout;
