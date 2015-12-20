1039702dfa039ff5d8204e42273c0ae14b086de2
kevinhennessy <marykevinh@gmail.com>
2015-12-20 18:36:00 -0500

    Upgrade to Angular 2 Beta

--- guessthenumber/app.ts
+++ guessthenumber/app.ts
@@ -1,6 +1,8 @@
-import {
-Component, View, bootstrap
-}from 'angular2/angular2';
+//import 'zone.js';
+//import 'reflect-metadata';
+
+import {bootstrap} from 'angular2/platform/browser';
+import {Component} from 'angular2/core';
 
 @Component({
   selector: 'my-app',
