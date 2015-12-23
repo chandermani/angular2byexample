e6e86762ed5bccbb205185677148c4d62c585f81
chandermani <chandermani@outlook.com>
2015-12-23 09:47:41 +0530

    Moved the html template to app.ts

--- trainer/src/components/app/app.ts
+++ trainer/src/components/app/app.ts
@@ -6,7 +6,16 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 @Component({
   selector: 'trainer-app',
   directives: [ROUTER_DIRECTIVES],
-  template: `<router-outlet></router-outlet>`
+  template: `<div class="navbar navbar-default navbar-fixed-top top-navbar">
+              <div class="container app-container">
+                <div class="navbar-header">
+                  <h1>7 Minute Workout</h1>
+                </div>
+              </div>
+            </div>
+            <div class="container body-content app-container">
+              <router-outlet></router-outlet>
+            </div>`
 })
 @RouteConfig([
   { path: '/start', name: 'Start', component: Start, useAsDefault: true },
