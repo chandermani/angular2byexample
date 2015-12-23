8f86f73360fe145276bf9609a02f40900fb05015
chandermani <chandermani@outlook.com>
2015-12-23 08:56:27 +0530

    Added local-storage support for history save.

--- trainer/src/bootstrap.ts
+++ trainer/src/bootstrap.ts
@@ -2,4 +2,5 @@ import {bootstrap} from 'angular2/platform/browser';
 import {TrainerApp} from './components/app/app';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {WorkoutHistoryTracker} from './services/workout-history-tracker';
-bootstrap(TrainerApp, [ROUTER_PROVIDERS, WorkoutHistoryTracker]);
+import {LocalStorage} from './services/local-storage';
+bootstrap(TrainerApp, [ROUTER_PROVIDERS, WorkoutHistoryTracker, LocalStorage]);
--- trainer/src/services/workout-history-tracker.ts
+++ trainer/src/services/workout-history-tracker.ts
@@ -1,9 +1,18 @@
 import {ExercisePlan} from '../components/workout-runner/model';
+import {LocalStorage} from './local-storage';
+import {Injectable} from 'angular2/core';
+
+@Injectable()
 export class WorkoutHistoryTracker {
   private _maxHistoryItems: number = 20;   //We only track for last 20 exercise
   private _currentWorkoutLog: WorkoutLogEntry = null;
   private _workoutHistory: Array<WorkoutLogEntry> = [];
   private _tracking: boolean;
+  private _storageKey: string = "workouts";
+
+  constructor(private _storage: LocalStorage) {
+    this._workoutHistory=_storage.getItem<Array<WorkoutLogEntry>>(this._storageKey) || [];
+  }
 
   get tracking(): boolean {
     return this._tracking;
@@ -16,11 +25,13 @@ export class WorkoutHistoryTracker {
       this._workoutHistory.shift();
     }
     this._workoutHistory.push(this._currentWorkoutLog);
+    this._storage.setItem(this._storageKey, this._workoutHistory);
   }
 
   exerciseComplete(exercise: ExercisePlan) {
     this._currentWorkoutLog.lastExercise = exercise.exercise.title;
     ++this._currentWorkoutLog.exercisesDone;
+    this._storage.setItem(this._storageKey, this._workoutHistory);
   }
 
   endTracking(completed: boolean) {
@@ -28,6 +39,7 @@ export class WorkoutHistoryTracker {
     this._currentWorkoutLog.endedOn = new Date().toISOString();
     this._currentWorkoutLog = null;
     this._tracking = false;
+    this._storage.setItem(this._storageKey, this._workoutHistory);
   };
 
   getHistory(): Array<WorkoutLogEntry> {
