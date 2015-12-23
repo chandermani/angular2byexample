7bfe69be247a1de5ec8727a087caee962b28f711
chandermani <chandermani@outlook.com>
2015-12-23 09:59:09 +0530

    Moved the index template to app component. Index now only has trainer-app

--- trainer/src/components/app/app.ts
+++ trainer/src/components/app/app.ts
@@ -3,7 +3,16 @@ import {WorkoutRunner} from '../workout-runner/workout-runner';
 @Component({
     selector: 'trainer-app',
     directives:[WorkoutRunner],
-    template: `<workout-runner></workout-runner>`
+    template: `<div class="navbar navbar-default navbar-fixed-top top-navbar">
+                <div class="container app-container">
+                  <div class="navbar-header">
+                    <h1>7 Minute Workout</h1>
+                  </div>
+                </div>
+              </div>
+              <div class="container body-content app-container">
+                <workout-runner></workout-runner>
+              </div>`
 })
 export class TrainerApp {
     name: string = 'World';
