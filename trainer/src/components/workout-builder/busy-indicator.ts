c6ab3f87e19ec45abfc49443b314dc414dc4c608
chandermani <chandermani@outlook.com>
2016-02-14 13:01:55 +0530

    Improved busy indicator implementation.

--- trainer/src/components/workout-builder/busy-indicator.ts
+++ trainer/src/components/workout-builder/busy-indicator.ts
@@ -6,35 +6,9 @@ import {NgControl} from 'angular2/common';
 })
 export class BusyIndicator {
   private _subscriptions: Array<any> = [];
-  @HostBinding("readonly") validating: boolean;
-  constructor(private _element: ElementRef, private _renderer: Renderer, private _control: NgControl) { }
+  private get _validating(): boolean { return this._control.control != null && this._control.control.pending; };
+  @HostBinding("style.borderWidth") get controlBorderWidth(): string { return this._validating ? "3px" : null; };
+  @HostBinding("style.borderColor") get controlBorderColor(): string { return this._validating ? "gray" : null };
 
-  ngOnDestroy() {
-    this._subscriptions.forEach((s) => s.unsubscribe());
-  }
-
-  ngAfterContentChecked() {
-    if (this._control.control != null && this._subscriptions.length == 0) {
-      this._subscriptions.push(this._control.control
-        .statusChanges
-        .debounceTime(500)
-        .subscribe((status: any) => {
-        if (this._control.control.pending) {
-          this._renderer.setElementStyle(this._element, "border-width", "2");
-          this._renderer.setElementStyle(this._element, "border-color", "gray");
-          this.validating = true;
-        }
-        else {
-          this._renderer.setElementStyle(this._element, "border-width", null);
-          this._renderer.setElementStyle(this._element, "border-color", null);
-          this.validating = false;
-        }
-      }));
-    }
-  }
-
-  ngAfterContentInit() {
-    console.log('ngAfterContentInit');
-    // The control property for ngControl is not available in this event to attach event listners.
-  }
+  constructor(private _control: NgControl) { }
 }
