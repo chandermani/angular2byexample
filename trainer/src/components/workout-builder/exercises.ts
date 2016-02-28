9ebedec27b9a859ad50a9dc4904ff149fc5d0f67
kevinhennessy <marykevinh@gmail.com>
2016-02-27 22:06:53 -0500

    Add exercises.ts

--- trainer/src/components/workout-builder/exercises.ts
+++ trainer/src/components/workout-builder/exercises.ts
@@ -13,22 +13,28 @@ import {WorkoutService} from "../../services/workout-service";
 })
 export class Exercises {
     public exerciseList:Array<Exercise> = [];
+    public errorMessage: any;
 
     constructor(private _router:Router,
                 private _workoutService:WorkoutService) {
     }
 
     ngOnInit() {
-        this.exerciseList = this._workoutService.getExercises();
-        this.exerciseList.sort((n1,n2) => {
-            if (n1.title > n2.title) {
-                return 1;
-            }
-            if (n1.title < n2.title) {
-                return -1;
-            }
-            return 0;
-        });
+        this._workoutService.getExercises()
+            .subscribe(
+                exerciseList=> {
+                    this.exerciseList = exerciseList.sort((n1,n2) => {
+                        if (n1.title > n2.title) {
+                            return 1;
+                        }
+                        if (n1.title < n2.title) {
+                            return -1;
+                        }
+                        return 0;
+                    });
+                },
+                (err: any) => console.error(err)
+            );
     }
 
     onSelect(exercise:Exercise) {
