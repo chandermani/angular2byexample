18685ecb9225bfa82e85694ebd2ce84fe697fd2d
kevinhennessy <marykevinh@gmail.com>
2016-04-11 20:06:34 -0400

    Update to Angular 2.0.0-beta.14

--- trainer/src/bootstrap.ts
+++ trainer/src/bootstrap.ts
@@ -1,4 +1,3 @@
-///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 import {bootstrap} from 'angular2/platform/browser';
 import {TrainerApp} from './components/app/app';
 bootstrap(TrainerApp);
