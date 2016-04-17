dc09f21302fbed837532abda991826f64d1a94db
chandermani <chandermani@outlook.com>
2016-04-17 13:38:02 +0530

    Fixed SPA HTML5 mode. C'hanged gulp to use gulp-connect.
    # Conflicts:
    #	trainer/index.html

--- trainer/src/bootstrap.ts
+++ trainer/src/bootstrap.ts
@@ -1,3 +1,4 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 import {bootstrap} from 'angular2/platform/browser';
 import {TrainerApp} from './components/app/app';
 import {ROUTER_PROVIDERS} from 'angular2/router';
