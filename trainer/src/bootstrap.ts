9bfaa04479a508665bb51111d0f8beb311764ab4
chandermani <chandermani@outlook.com>
2016-04-24 18:32:54 +0530

    Converted history to a dialog.

--- trainer/src/bootstrap.ts
+++ trainer/src/bootstrap.ts
@@ -1,3 +1,4 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 import {bootstrap} from 'angular2/platform/browser';
 import {TrainerApp} from './components/app/app';
 import 'rxjs/Rx';
@@ -5,7 +6,10 @@ import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {WorkoutHistoryTracker} from './services/workout-history-tracker';
 import {LocalStorage} from './services/local-storage';
+import {provide} from 'angular2/core';
+import {Modal, ModalConfig} from 'angular2-modal';
 import {WorkoutService} from "./services/workout-service";
 import {WorkoutBuilderService} from "./services/workout-builder-service";
 import {ExerciseBuilderService} from "./services/exercise-builder-service";
-bootstrap(TrainerApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS, WorkoutHistoryTracker, LocalStorage, WorkoutBuilderService, WorkoutService, ExerciseBuilderService]);
+
+bootstrap(TrainerApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS, WorkoutHistoryTracker, LocalStorage, WorkoutBuilderService, WorkoutService, ExerciseBuilderService, provide(ModalConfig, { useValue: new ModalConfig('lg', true, 81) }), Modal]);
