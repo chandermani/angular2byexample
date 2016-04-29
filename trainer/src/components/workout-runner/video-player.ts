724245b30140339b9395c6cf3635b86862cd8ece
chandermani <chandermani@outlook.com>
2016-04-29 09:12:13 +0530

    Removed event related implementation for video-player. Will come in checkpoint3.3

--- trainer/src/components/workout-runner/video-player.ts
+++ trainer/src/components/workout-runner/video-player.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter, Injector, provide} from 'angular2/core';
+import {Component, Input, Injector, provide} from 'angular2/core';
 import {Modal, ModalConfig} from 'angular2-modal';
 import {VideoDialog} from './video-dialog';
 
@@ -8,19 +8,13 @@ import {VideoDialog} from './video-dialog';
 })
 export class VideoPlayer {
   @Input() videos: Array<string>;
-  @Output() playbackStarted: EventEmitter<any> = new EventEmitter<any>();
-  @Output() playbackEnded: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(private _modal: Modal) { }
 
   playVideo(videoId:string) {
-    this.playbackStarted.emit(null);
     let resolvedBindings = Injector.resolve([provide('videoId', { useValue: videoId })]);
-    var dialog = this._modal.open(<any>VideoDialog,
+    this._modal.open(<any>VideoDialog,
       resolvedBindings,
       new ModalConfig('lg', true, 27));
-    dialog
-      .then((d) => d.result)
-      .then(() => { this.playbackEnded.emit(null); }, (error) => { this.playbackEnded.emit(null); });
   };
 }
