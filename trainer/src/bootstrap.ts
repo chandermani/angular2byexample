572962b4c8bfcd27082a9c120a77ec83510a305b
chandermani <chandermani@outlook.com>
2016-05-09 10:01:30 +0530

    Upgraded to Angular 2 rc1

--- trainer/src/bootstrap.ts
+++ trainer/src/bootstrap.ts
@@ -1,5 +1,3 @@
-/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
-
-import {bootstrap} from 'angular2/platform/browser';
+import {bootstrap} from '@angular/platform-browser-dynamic';
 import {TrainerApp} from './components/app/app';
 bootstrap(TrainerApp);
