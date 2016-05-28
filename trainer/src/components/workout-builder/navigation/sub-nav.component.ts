1e92986894137c6b0d0e41c95ef76913db3b88cd
Kevin Hennessy <marykevinh@gmail.com>
2016-05-28 15:10:33 -0400

    Update sub-nav.component.ts
    Fix selector name

--- trainer/src/components/workout-builder/navigation/sub-nav.component.ts
+++ trainer/src/components/workout-builder/navigation/sub-nav.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 @Component({
-    selector: 'top-nav',
+    selector: 'sub-nav',
     templateUrl: '/src/components/workout-builder/top-nav.component.html',
     directives: [ROUTER_DIRECTIVES]
 })
