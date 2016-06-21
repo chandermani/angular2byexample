6cfb087d3f9e97be21b3374b0244e5709a697a43
chandermani <chandermani@outlook.com>
2016-06-21 21:03:12 +0530

    Fixed checkpoint2.3 video-player, to sanitize video content using DomSanitizationService

--- trainer/src/components/workout-runner/video-player/video-player.component.ts
+++ trainer/src/components/workout-runner/video-player/video-player.component.ts
@@ -1,9 +1,21 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
+import {DomSanitizationService, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'video-player',
   templateUrl: '/src/components/workout-runner/video-player/video-player.html'
 })
-export class VideoPlayerComponent {
+export class VideoPlayerComponent implements OnChanges {
+  private youtubeUrlPrefix = '//www.youtube.com/embed/';
+
   @Input() videos: Array<string>;
+  @Input() safeVideoUrls: Array<SafeResourceUrl>;
+
+  constructor(private sanitizer: DomSanitizationService) { }
+
+  ngOnChanges() {
+    this.safeVideoUrls = this.videos ?
+      this.videos.map(v => this.sanitizer.bypassSecurityTrustResourceUrl(this.youtubeUrlPrefix + v))
+      : this.videos;
+  }
 }
