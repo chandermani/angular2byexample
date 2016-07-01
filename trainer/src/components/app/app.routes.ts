8b33c5aefbd8193b024a55ab5c2d423f41f120e6
chandermani <chandermani@outlook.com>
2016-07-01 08:13:58 +0530

    Upgraded to angular rc4
    # Conflicts:
    #	trainer/package.json

--- trainer/src/components/app/app.routes.ts
+++ trainer/src/components/app/app.routes.ts
@@ -2,11 +2,13 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import {WorkoutRunnerComponent} from '../workout-runner/workout-runner.component';
 import {StartComponent} from '../start/start.component';
 import {FinishComponent} from '../finish/finish.component';
+import {WorkoutHistoryComponent} from '../workout-history/workout-history.component';
 
 export const routes: RouterConfig = [
     { path: 'start', component: StartComponent },
     { path: 'workout', component: WorkoutRunnerComponent },
     { path: 'finish', component: FinishComponent },
+    { path: 'history', component: WorkoutHistoryComponent },
     { path: '', component: StartComponent }
 ]
 ;
