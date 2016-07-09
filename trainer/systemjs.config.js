eceecbe07d4f6caa7de5918700fadfd505bc9018
chandermani <chandermani@outlook.com>
2016-07-09 15:29:23 +0530

    Updated systemjs to support bundled umd files

--- trainer/systemjs.config.js
+++ trainer/systemjs.config.js
@@ -1,33 +1,45 @@
-(function(global) {
+(function (global) {
 
   // map tells the System loader where to look for things
   var map = {
-    'app':                        'dist', // 'dist',
-    'rxjs':                       'node_modules/rxjs',
-    '@angular':                   'node_modules/@angular'
+    'app': 'dist', // 'dist',
+    'rxjs': 'node_modules/rxjs',
+    '@angular': 'node_modules/@angular'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main:'bootstrap.js' ,defaultExtension: 'js' },
-    'rxjs':                       { defaultExtension: 'js' }
+    'app': { main: 'bootstrap.js', defaultExtension: 'js' },
+    'rxjs': { defaultExtension: 'js' }
   };
 
-  var packageNames = [
-    '@angular/common',
-    '@angular/compiler',
-    '@angular/core',
-    '@angular/http',
-    '@angular/platform-browser',
-    '@angular/platform-browser-dynamic',
-    '@angular/router',
-    '@angular/testing'
+  var ngPackageNames = [
+    'common',
+    'compiler',
+    'core',
+    'http',
+    'platform-browser',
+    'platform-browser-dynamic',
+    'router',
+    'testing'
   ];
 
+  // Individual files (~300 requests):
+  function packIndex(pkgName) {
+    packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  }
+
   // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
-  packageNames.forEach(function(pkgName) {
-    packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
-  });
+  // Bundled (~40 requests):
+  function packUmd(pkgName) {
+    packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+  }
+
+  // Most environments should use UMD; some (Karma) need the individual index files
+  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
+  // Add package entries for angular packages
+  ngPackageNames.forEach(setPackageConfig);
 
   var config = {
     map: map,
