02af994902c11b5e3f2f7448ee6870cd7aa5b0bf
chandermani <chandermani@outlook.com>
2016-07-29 10:16:56 +0530

    Fixed selectors and removed NgControl

--- trainer/src/components/workout-builder/shared/busy-indicator.directive.ts
+++ trainer/src/components/workout-builder/shared/busy-indicator.directive.ts
@@ -1,8 +1,8 @@
 import {Directive, HostBinding} from '@angular/core';
-import {NgControl} from '@angular/forms';
+import {NgModel} from '@angular/forms';
 
 @Directive({
-  selector: `[a2beBusyIndicator][ngControl],[a2beBusyIndicator][ngModel]`,
+  selector: `[a2beBusyIndicator][ngModel]`,
 })
 export class BusyIndicatorDirective {
   private _subscriptions: Array<any> = [];
@@ -10,5 +10,5 @@ export class BusyIndicatorDirective {
   @HostBinding("style.borderWidth") get controlBorderWidth(): string { return this._validating ? "3px" : null; };
   @HostBinding("style.borderColor") get controlBorderColor(): string { return this._validating ? "gray" : null };
 
-  constructor(private _control: NgControl) { }
+  constructor(private _control: NgModel) { }
 }
--- trainer/src/components/workout-builder/shared/remote-validator.directive.ts
+++ trainer/src/components/workout-builder/shared/remote-validator.directive.ts
@@ -2,7 +2,7 @@ import {provide, Directive, Input} from '@angular/core';
 import { NG_ASYNC_VALIDATORS, Validators, Validator, FormControl } from '@angular/forms';
 
 @Directive({
-  selector: `[a2beRemoteValidator][ngControl],[a2beRemoteValidator][ngModel]`,
+  selector: `[a2beRemoteValidator][ngModel]`,
   providers: [
     provide(NG_ASYNC_VALIDATORS, {
       useExisting: RemoteValidatorDirective,
