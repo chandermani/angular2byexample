f8fdc08a7b462be6530e9ab21ae65701baa8ab41
chandermani <chandermani@outlook.com>
2016-07-30 16:05:19 +0530

    Updated ES6 typing definition to use core-js

--- trainer/src/bootstrap.ts
+++ trainer/src/bootstrap.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/browser.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
