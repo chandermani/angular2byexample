0f0a498b3f62289058646f38998010f8f2dcd2d0
chandermani <chandermani@outlook.com>
2016-07-31 09:15:31 +0530

    Renamed _control to model

--- trainer/src/components/workout-builder/shared/busy-indicator.directive.ts
+++ trainer/src/components/workout-builder/shared/busy-indicator.directive.ts
@@ -5,10 +5,9 @@ import {NgModel} from '@angular/forms';
   selector: `[a2beBusyIndicator][ngModel]`,
 })
 export class BusyIndicatorDirective {
-  private _subscriptions: Array<any> = [];
-  private get _validating(): boolean { return this._control.control != null && this._control.control.pending; };
+  private get _validating(): boolean { return this.model.control != null && this.model.control.pending; };
   @HostBinding("style.borderWidth") get controlBorderWidth(): string { return this._validating ? "3px" : null; };
   @HostBinding("style.borderColor") get controlBorderColor(): string { return this._validating ? "gray" : null };
 
-  constructor(private _control: NgModel) { }
+  constructor(private model: NgModel) { }
 }
