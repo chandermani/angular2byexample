bbba00d5cd07ec6bd4fe3104ed1353aff9b95933
chandermani <chandermani@outlook.com>
2016-08-02 07:45:06 +0530

    Updated ES6 typing definition to use core-js and removed extra video-dialog.ts
    # Conflicts:
    #	trainer/src/bootstrap.ts

--- trainer/src/bootstrap.ts
+++ trainer/src/bootstrap.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/browser.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 import { bootstrap } from '@angular/platform-browser-dynamic';
 
 import { LocalStorage } from './services/local-storage';
