ea33b1c1591f5cd41fd527ef49d95e50c9b53674
kevinhennessy <marykevinh@gmail.com>
2016-08-16 16:18:18 -0400

    Merge remote-tracking branch 5.4 into checkpoint6.1

--- trainer/src/components/workout-builder/workout/workout.component.ts
+++ trainer/src/components/workout-builder/workout/workout.component.ts
@@ -23,20 +23,22 @@ export class WorkoutComponent implements OnInit, OnDestroy {
     private submitted: boolean = false;
     private removeTouched: boolean = false;
     private isExistingWorkout: boolean = false
+    private workoutName: string;
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
-        private workoutBuilderService:WorkoutBuilderService){ }
+        private workoutBuilderService:WorkoutBuilderService,
+        private workoutService: WorkoutService){ }
 
     ngOnInit() {
         this.sub = this.route.params.subscribe(params => {
             if (!params['id']) {
                 this.workout = this.workoutBuilderService.startBuildingNew();
             } else {
-                let workoutName = params['id'];
+                this.workoutName = params['id'];
                 this.isExistingWorkout = true;
-                this.workoutBuilderService.startBuildingExisting(workoutName)
+                this.workoutBuilderService.startBuildingExisting(this.workoutName)
                     .subscribe(
                     (data: WorkoutPlan) => {
                         this.workout = <WorkoutPlan>data;
@@ -95,26 +97,6 @@ export class WorkoutComponent implements OnInit, OnDestroy {
             });
     }
 
-    durations = [{ title: "15 seconds", value: 15 },
-        { title: "30 seconds", value: 30 },
-        { title: "45 seconds", value: 45 },
-        { title: "1 minute", value: 60 },
-        { title: "1 minute 15 seconds", value: 75 },
-        { title: "1 minute 30 seconds", value: 90 },
-        { title: "1 minute 45 seconds", value: 105 },
-        { title: "2 minutes", value: 120 },
-        { title: "2 minutes 15 seconds", value: 135 },
-        { title: "2 minutes 30 seconds", value: 150 },
-        { title: "2 minutes 45 seconds", value: 165 },
-        { title: "3 minutes", value: 180 },
-        { title: "3 minutes 15 seconds", value: 195 },
-        { title: "3 minutes 30 seconds", value: 210 },
-        { title: "3 minutes 45 seconds", value: 225 },
-        { title: "4 minutes", value: 240 },
-        { title: "4 minutes 15 seconds", value: 255 },
-        { title: "4 minutes 30 seconds", value: 270 },
-        { title: "4 minutes 45 seconds", value: 285 },
-        { title: "5 minutes", value: 300 }];
     durations = [{ title: "15 seconds", value: 15 },
         { title: "30 seconds", value: 30 },
         { title: "45 seconds", value: 45 },
