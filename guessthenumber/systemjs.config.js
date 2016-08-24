fe0593400d8ef1c890864d80e5bc88f57b09a7a4
kevinhennessy <marykevinh@gmail.com>
2016-08-24 17:38:51 -0400

    Update GuessTheNumber to RC4

--- guessthenumber/systemjs.config.js
+++ guessthenumber/systemjs.config.js
@@ -2,15 +2,19 @@ System.config({
     map : {
         'app': 'app',
         'rxjs': 'https://npmcdn.com/rxjs@5.0.0-beta.6',
-        '@angular': 'https://npmcdn.com/@angular'
+        '@angular/common': 'https://npmcdn.com/@angular/common@2.0.0-rc.4',
+        '@angular/compiler': 'https://npmcdn.com/@angular/compiler@2.0.0-rc.4',
+        '@angular/core': 'https://npmcdn.com/@angular/core@2.0.0-rc.4',
+        '@angular/platform-browser': 'https://npmcdn.com/@angular/platform-browser@2.0.0-rc.4',
+        '@angular/platform-browser-dynamic': 'https://npmcdn.com/@angular/platform-browser-dynamic@2.0.0-rc.4'
     },
     packages:{
         'app':  { main: 'main.ts',  defaultExtension: 'ts' },
-        '@angular/common': { main: 'index.js' },
-        '@angular/compiler': { main: 'index.js' },
-        '@angular/core': { main: 'index.js' },
-        '@angular/platform-browser': { main: 'index.js' },
-        '@angular/platform-browser-dynamic': { main: 'index.js' },
+        '@angular/common': { main: 'bundles/common.umd.js', defaultExtension: 'js' },
+        '@angular/compiler': { main: 'bundles/compiler.umd.js', defaultExtension: 'js' },
+        '@angular/core': { main: 'bundles/core.umd.js', defaultExtension: 'js' },
+        '@angular/platform-browser': { main: 'bundles/platform-browser.umd.js', defaultExtension: 'js' },
+        '@angular/platform-browser-dynamic': { main: 'bundles/platform-browser-dynamic.umd.js', defaultExtension: 'js' },
     },
     // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
     transpiler: 'typescript',
