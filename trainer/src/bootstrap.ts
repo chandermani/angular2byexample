fd5e85579afa4e4154cdce3c8a96891bd7275a25
kevinhennessy <marykevinh@gmail.com>
2016-08-25 13:18:37 -0400

    Delete WorkoutTrainer base

--- trainer/src/bootstrap.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../typings/index.d.ts" />
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {TrainerAppComponent} from './components/app/app.component';
-bootstrap(TrainerAppComponent);
