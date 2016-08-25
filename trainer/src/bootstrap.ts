ced4ff977719e449ada7d6845a21151ab5702df4
kevinhennessy <marykevinh@gmail.com>
2016-08-25 13:20:31 -0400

    Delete WorkoutTrainer from master

--- trainer/src/bootstrap.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {TrainerApp} from './components/app/app';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-bootstrap(TrainerApp, [ROUTER_PROVIDERS]);
