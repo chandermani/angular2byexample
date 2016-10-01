b97f8be2b155d18450142ae9661b07c68a71658c
kevinhennessy <marykevinh@gmail.com>
2016-10-01 15:52:06 -0400

    Change import in Workout Builder Module from BrowserModule to CommonModule

--- trainer/src/components/workout-builder/workout-builder.module.ts
+++ trainer/src/components/workout-builder/workout-builder.module.ts
@@ -1,10 +1,10 @@
 import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { WorkoutBuilderComponent } from "./workout-builder.component";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [CommonModule],
     declarations: [
         WorkoutBuilderComponent,
     ],
